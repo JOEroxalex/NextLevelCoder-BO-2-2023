e616762721aeadf1dd185e8fca939a57f7041262
Joel Rosales <Joel.Rosales@fundacion-jala.org>
2023-03-30 19:25:43 -0400

    clase 1: clase inicial

--- main.py
+++ main.py
@@ -3,4 +3,15 @@ from dino_runner.components.game import Game
 if __name__ == "__main__":
     game = Game()
     game.run()
-    print("hello there...")
+    
+
+
+## tareas
+
+ # investigar que tipo de dato es (2,2) (255,255,255)
+
+ # investiga herencia 
+ # investigar que es Sprite (juegos)
+ # agregar las nubes
+
+ # investigar que hace el metodo draw_background(self)
